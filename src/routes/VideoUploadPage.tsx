7b68ace7df24cef8d78ed525c898d1dea42107c6
Maria Stashenko <mahryia@gmail.com>
2026-06-08 12:09:21 +0300

    add video upload page

--- src/routes/VideoUploadPage.tsx
+++ src/routes/VideoUploadPage.tsx
@@ -1,3 +1,12 @@
+import { VideoUploader } from '../features/videos/components/VideoUploader';
+
 export function VideoUploadPage() {
-	return <div>Video Upload Page</div>;
+  return (
+    <div>
+      <h1 className="text-3xl font-bold mb-8">Upload Video</h1>
+      <div className="max-w-2xl">
+        <VideoUploader />
+      </div>
+    </div>
+  );
 }
